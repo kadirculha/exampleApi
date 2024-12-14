3571430dae1f049beaa14d242eed005eacb2ac2f
Abdulkadir <84346396+kadirculha@users.noreply.github.com>
2024-12-15 02:33:48 +0300

    Update restart.sh

--- sh_files/restart.sh
+++ sh_files/restart.sh
@@ -1,12 +1,3 @@
 docker rm -f exampleapi
 
-# Remove any existing container named 'exampleapi' forcefully
-docker rm -f exampleapi
-
-# Run a new Docker container in the background with the following options:
-docker run -d \            # Run the container in detached mode (in the background)
-    -ti \                  # Allocate a pseudo-TTY and keep it open even if not attached
-    --restart=always \     # Always restart the container unless explicitly stopped
-    --name exampleapi \    # Assign the name 'exampleapi' to the container
-    -p 8010:8010 \         # Map port 8010 of the host to port 8010 of the container
-    exampleapi             # Use the image named 'exampleapi'
+docker run -d -ti --restart=always --name exampleapi -p 8010:8010 exampleapi
